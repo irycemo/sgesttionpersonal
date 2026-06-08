68b2ba187cd1850068097a124e4af52d286418a8
kikorobledo <enrique_j_@hotmail.com>
2026-06-08 16:13:12 -0600

    Se quita restriccion a rfc y curp

--- app/Livewire/Personal/Personal.php
+++ app/Livewire/Personal/Personal.php
@@ -56,12 +56,12 @@ class Personal extends Component
             'foto' => 'nullable|mimes:jpg,png,jpeg',
             'modelo_editar.rfc' => [
                         'unique:personas,rfc,' . $this->modelo_editar->id,
-                        'required',
+                        'nullable',
                         'regex:/^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/'
                     ],
             'modelo_editar.curp' => [
                         'unique:personas,curp,' . $this->modelo_editar->id,
-                        'required',
+                        'nullable',
                         'regex:/^[A-Z]{1}[AEIOUX]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/i'
                     ],
          ];
